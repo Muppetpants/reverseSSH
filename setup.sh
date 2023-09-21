ee535c65792c7262329d148fe1b3ccdd40557e58
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2023-09-21 16:46:48 -0400

    Update setup.sh
    Added padding, removed jumpbox.sh and added  contents to cronjob

--- setup.sh
+++ setup.sh
@@ -21,9 +21,9 @@ read -n 1 -r -s -p $'Press enter to continue if the values above are correct. Ot
 
 
 #Cleanup
-rm /home/$piUser/jumpbox.sh 2> /dev/null
 rm /home/$piUser/.ssh/piTO$vpsIP 2> /dev/null
 rm /home/$PiUser/.ssh/piTO$vpsIP.pub 2> /dev/null
+sudo sh -c  "rm /etc/cron.d/revSSHcronjob"
 
 
 
@@ -31,20 +31,15 @@ rm /home/$PiUser/.ssh/piTO$vpsIP.pub 2> /dev/null
 ssh-keygen -t ecdsa -b 384 -f /home/$piUser/.ssh/piTO$vpsIP -q -N ""
 ssh-copy-id -i /home/$piUser/.ssh/piTO$vpsIP.pub -p $vpsPort $vpsUser@$vpsIP
 # Will be prompted for VPS authentication 
-
 sleep 1
 
-#Create startup script with RevSSH vars
-echo "ssh -fN -R $piRevPort:localhost:$piPort $vpsUser@$vpsIP -p $vpsPort -i /home/$piUser/.ssh/piTO$vpsIP">> /home/$piUser/jumpbox.sh
-chmod +x /home/$piUser/jumpbox.sh
-
 #Create cronjob to start tunnel at reboot
-sudo sh -c  "echo @reboot $piUser sleep 90 \&\& bash /home/$piUser/jumpbox.sh > /etc/cron.d/cronjob"
-sudo sh -c "chmod 0644 /etc/cron.d/cronjob"
+sudo sh -c  "echo @reboot $piUser sleep 90 \&\& ssh -fN -R $piRevPort:localhost:$piPort $vpsUser@$vpsIP -p $vpsPort -i /home/$piUser/.ssh/piTO$vpsIP > /etc/cron.d/revSSHcronjob"
+sudo sh -c "chmod 0644 /etc/cron.d/revSSHcronjob"
 
 
 #Ensure new key works and provides remote access
 echo "######################## Test cert and gain access ########################"
-echo "[1] Test access: ssh $vpsUser@$vpsIP -p $vpsPort -i /home/$piUser/.ssh/piTO$vpsIP"
-echo "[2] Reboot pi: sudo reboot"
-echo "[3] From VPS, run: ssh $piUser@localhost -p $piRevPort  #Use $piUser password."
+echo "[1] Test access. Run: ssh $vpsUser@$vpsIP -p $vpsPort -i /home/$piUser/.ssh/piTO$vpsIP"
+echo "[2] Reboot pi. Run: sudo reboot"
+echo "[3] Login to VPS, then access Pi. Run: ssh $piUser@localhost -p $piRevPort"
