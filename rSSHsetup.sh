84d0fc6b23b3fc12b5fb38d7d2c1ef8b1655e914
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2023-10-30 15:38:52 -0400

    Update and rename setup.sh to rSSHsetup.sh

--- rSSHsetup.sh
+++ rSSHsetup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #Description: Run script on a pi to create a VPS (deb/ubuntu) callback through SSH
+#Usage: On RPi with Internet access, run ./rSSHsetup.sh and provide necessary RPi and VPS details to configure.
 
 clear
 # Checks to verify that the script is not running as root
 if [[ $EUID -eq 0 ]]; then
    echo "THIS SCRIPT SHOULD NOT BE RUN AS ROOT."
-   echo "EX:  ./setup.sh"
+   echo "EX:  ./rSSHsetup.sh"
    exit 1
 fi
 
